fa1613002758aa3cebe67dbd96ba0f3088575f1f
Jeremy Ellington <jme@eminor.net>
2024-08-04 20:08:23 -0500

    initial commit of poc.py

---
+++ poc.py
@@ -0,0 +1,66 @@
+"""
+Install the Google AI Python SDK
+
+$ pip install google-generativeai
+
+See the getting started guide for more information:
+https://ai.google.dev/gemini-api/docs/get-started/python
+"""
+
+import os
+
+import google.generativeai as genai
+
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+# Create the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+  generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
+def upload_to_gemini(path, mime_type=None):
+  """Uploads the given file to Gemini.
+
+  See https://ai.google.dev/gemini-api/docs/prompting_with_media
+  """
+  file = genai.upload_file(path, mime_type=mime_type)
+  print(f"Uploaded file '{file.display_name}' as: {file.uri}")
+  return file
+
+files = [
+  upload_to_gemini("image_what_shape_comes_next1.jpeg", mime_type="image/jpeg"),
+]
+
+# chat_session = model.start_chat(
+#   history=[
+#     {
+#       "role": "user",
+#       "parts": [
+#         files[0],
+#         "Look at this sequence of three shapes. What shape should come as the fourth shape? Explain your reasoning with detailed descriptions of the first shapes.",
+#       ],
+#     },
+#     {
+#       "role": "model",
+#       "parts": [
+#         "The sequence is triangle, square, pentagon. The number of sides in each shape increases by one. Therefore, the next shape should be a hexagon. \n",
+#       ],
+#     },
+#   ]
+# )
+chat_session = model.start_chat()
+
+response = chat_session.send_message("Look at this sequence of three shapes. What shape should come as the fourth shape? Explain your reasoning with detailed descriptions of the first shapes.")
+
+print(response)
